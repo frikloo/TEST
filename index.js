96337816381adc9ab4dd0be93a4274ae2b7a914b
frikloo <hamzazour2@gmail.com>
2026-03-02 13:17:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('events').EventEmitter.prototype._maxListeners = 50;
 const { Client } = require('discord.js-selfbot-v13');
 const { joinVoiceChannel } = require('@discordjs/voice');
 
-const channels = ['730459316238155856'];
+const channels = ['1443933520895676611'];
 
 const checkAndJoin = async (client, channelId) => {
   try {
